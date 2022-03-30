b3716ee47c10e2b6e14265a12e1ab1e857fb9769
isabelleoktay <60681704+isabelleoktay@users.noreply.github.com>
2022-03-30 13:22:28 -0400

    new browse classes backend

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -8,5 +9,11 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'test')));
+
 
 app.listen(port);
+
+app.get('/browse', (res, req, next) => {
+    res.send(path.join('classes', 'all.js'))
+})
