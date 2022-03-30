1390c4b3d7cd63cb0fa5fd4bd8696e1c90d10fba
isabelleoktay <60681704+isabelleoktay@users.noreply.github.com>
2022-03-30 13:31:08 -0400

    idk if its fixed

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 require('dotenv').config();
 const bodyParser = require('body-parser');
-const path = require('path');
+// const path = require('path');
+const allClasses = require('./test/classes/all.js')
 
 const app = express();
 
@@ -9,10 +10,10 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'test')));
+// app.use(express.static(path.join(__dirname, 'test')));
 
 app.listen(port);
 
 app.get('/browse', (res, req, next) => {
-    res.send(path.join('classes', 'all.js'))
+    res.send(allClasses)
 })
