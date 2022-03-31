240a23d1e2a07d8124c5d577afc0f08ca13a6eae
isabelleoktay <60681704+isabelleoktay@users.noreply.github.com>
2022-03-31 15:58:23 -0400

    my changes

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -7,7 +7,9 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/api', require('./routes'));
 
 app.listen(port);
